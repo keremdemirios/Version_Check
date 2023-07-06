d4948e443d37efff2933974b1db55ca835c4fe83
Kerem Demir <keremdemir@192.168.1.110>
2023-07-06 17:31:06 +0300

    Version check.

---
+++ Version_Check.playground/Contents.swift
@@ -0,0 +1,48 @@
+//It can be used under any class.
+
+enum VersionError: Error {
+case invalidResponse, invalidBundleInfo
+}
+
+@discardableResult
+func isUpdateAvailable(completion: @escaping (Bool?, Error?) -> Void) throws -> URLSessionDataTask {
+guard let info = Bundle.main.infoDictionary,
+    let currentVersion = info["CFBundleShortVersionString"] as? String,
+    let identifier = info["CFBundleIdentifier"] as? String,
+    let url = URL(string: "http://itunes.apple.com/lookup?bundleId=\(identifier)") else {
+        throw VersionError.invalidBundleInfo
+}
+    
+let request = URLRequest(url: url, cachePolicy: URLRequest.CachePolicy.reloadIgnoringLocalCacheData)
+
+let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+    do {
+        if let error = error { throw error }
+        
+        guard let data = data else { throw VersionError.invalidResponse }
+                    
+        let json = try JSONSerialization.jsonObject(with: data, options: [.allowFragments]) as? [String: Any]
+                    
+        guard let result = (json?["results"] as? [Any])?.first as? [String: Any], let lastVersion = result["version"] as? String else {
+            throw VersionError.invalidResponse
+        }
+        completion(lastVersion > currentVersion, nil)
+    } catch {
+        completion(nil, error)
+    }
+}
+
+task.resume()
+return task
+}
+
+
+
+// Usage :
+try? isUpdateAvailable {[self] (update, error) in
+    if let error = error {
+        print(error)
+    } else if update ?? false {
+        // show alert
+    }
+}
